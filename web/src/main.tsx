bc97a4a48e693215549d62af77e2caf257640c94
Stevens Castillo <stevenscastillog@gmail.com>
2026-05-11 18:32:09 -0500

    FIX RAIZ: main.tsx ya no envuelve App en BrowserRouter (router anidado). App.tsx usa RouterProvider custom.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster position="top-center" toastOptions={{ style: { background: '#1e293b', color: '#fff', border: '1px solid #334155' } }} />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
