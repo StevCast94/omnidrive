d1656661f02d1b10d02dee792e4c57f8d80181b0
Stevens Castillo <stevenscastillog@gmail.com>
2026-05-21 22:32:19 -0500

    fix: desregistrar service workers viejos al cargar la app

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Limpiar cualquier Service Worker previo que pueda estar cacheando versiones viejas
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const registration of registrations) {
+      registration.unregister();
+      console.log('[SW] Unregistered old service worker');
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
